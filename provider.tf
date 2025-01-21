c75b3fdb26778c4fc4aea0133877d9b652787e8a
Duy Duc Nguyen <82008595+loking4code@users.noreply.github.com>
2025-01-21 15:23:56 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "cmccloudv2" {
   api_endpoint = "https://apiv2.cloud.cmctelecom.vn"
   api_key      = " #Get API key from cmccloud portal 
-  project_id   = "90fa57e96942453d85dc907688ee82cf"         #Get project_id from IAM on portal 
+  project_id   = ""         #Get project_id from IAM on portal 
   region_id    = "hn-1"
 
 }
